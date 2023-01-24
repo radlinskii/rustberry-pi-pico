f835bc72fb405d1c21f8cd5f279f9f5aca0b0ef1
radlinskii <radlinskiignacy@gmail.com>
2023-01-24 19:02:17 +0100

    remove comments

--- src/examples/toggle-on-board-led-on-gpio-key-press/example.rs
+++ src/examples/toggle-on-board-led-on-gpio-key-press/example.rs
@@ -8,47 +8,22 @@
 #![no_std]
 #![no_main]
 
-// The macro for our start-up function
+use embedded_hal::digital::v2::{InputPin, OutputPin};
+use panic_halt as _;
 use rp_pico::entry;
+use rp_pico::hal;
+use rp_pico::hal::pac;
 use rp_pico::hal::prelude::*;
 
-// GPIO traits
-use embedded_hal::digital::v2::{InputPin, OutputPin};
-
-// Ensure we halt the program on panic (if we don't mention this crate it won't
-// be linked)
-use panic_halt as _;
-
-// A shorter alias for the Peripheral Access Crate, which provides low-level
-// register access
-use rp_pico::hal::pac;
-
-// A shorter alias for the Hardware Abstraction Layer, which provides
-// higher-level drivers.
-use rp_pico::hal;
-
-/// Entry point to our bare-metal application.
-///
-/// The `#[entry]` macro ensures the Cortex-M start-up code calls this function
-/// as soon as all global variables are initialised.
-///
-/// The function configures the RP2040 peripherals, then blinks the LED in an
-/// infinite loop.
 #[entry]
 fn main() -> ! {
-    // Grab our singleton objects
     let mut pac = pac::Peripherals::take().unwrap();
     let core = pac::CorePeripherals::take().unwrap();
 
-    // The single-cycle I/O block controls our GPIO pins
     let sio = hal::Sio::new(pac.SIO);
 
-    // Set up the watchdog driver - needed by the clock setup code
     let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
 
-    // Configure the clocks
-    //
-    // The default is to generate a 125 MHz system clock
     let clocks = hal::clocks::init_clocks_and_plls(
         rp_pico::XOSC_CRYSTAL_FREQ,
         pac.XOSC,
@@ -61,11 +36,8 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
-    // The delay object lets us wait for specified amounts of time (in
-    // milliseconds)
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
 
-    // Set the pins up according to their function on this particular board
     let pins = rp_pico::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
@@ -73,18 +45,14 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    // Set the LED to be an output
     let mut led_pin = pins.led.into_push_pull_output();
 
-    // Set one line of the button to be an input
     let button_input_pin = pins.gpio13.into_pull_up_input();
-    // Set second line of the button to be an output
     pins.gpio14.into_push_pull_output();
 
     let mut led_on = false;
     let mut prev_button_pressed = false;
 
-    // Light the LED when key is pressed
     loop {
         let button_pressed = button_input_pin.is_low().unwrap();
         if button_pressed && !prev_button_pressed {
