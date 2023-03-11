871215a74842772dd8fac03caa9686f51d2ff39f
Radliński Ignacy <radlinskiignacy@gmail.com>
2023-03-11 22:47:28 +0100

    feat: add one key keyboard example using picoprobe  (#30)
    * copy picoprobe-debugging example
    
    * add keyberon code
    
    * get logs in one key keyboard
    
    * add led indicating power on
    
    * feat: add readme

---
+++ src/examples/picoprobe-one-key-keyboard/example.rs
@@ -0,0 +1,178 @@
+//! # PicoProbe One Key Keyboard Example
+//!
+//! This example is a port of the keyberon-one-key-keyboard,
+//! but is using `picoprobe` for logging and debugging.
+//! It also handles interrupts differently, using software interrupts.
+
+#![no_std]
+#![no_main]
+
+use panic_probe as _;
+
+#[rtic::app(
+    device = rp_pico::hal::pac,
+    peripherals = true,
+    dispatchers = [SW0_IRQ, SW1_IRQ]
+)]
+mod app {
+    use defmt::*;
+    use defmt_rtt as _;
+    use embedded_hal::digital::v2::OutputPin;
+    use rp_pico::{
+        hal::{clocks, gpio::DynPin, sio::Sio, usb::UsbBus, watchdog::Watchdog},
+        XOSC_CRYSTAL_FREQ,
+    };
+
+    use keyberon::debounce::Debouncer;
+    use keyberon::key_code;
+    use keyberon::layout::{self, Layout};
+    use keyberon::matrix::Matrix;
+    use usb_device::class_prelude::*;
+    use usb_device::device::UsbDeviceState;
+
+    static mut USB_BUS: Option<usb_device::bus::UsbBusAllocator<rp2040_hal::usb::UsbBus>> = None;
+
+    pub static LAYERS: keyberon::layout::Layers<1, 1, 1> = keyberon::layout::layout! {
+        {
+            [
+                A
+            ]
+        }
+    };
+
+    #[shared]
+    struct Shared {
+        usb_dev: usb_device::device::UsbDevice<'static, rp2040_hal::usb::UsbBus>,
+        usb_class: keyberon::hid::HidClass<
+            'static,
+            rp2040_hal::usb::UsbBus,
+            keyberon::keyboard::Keyboard<()>,
+        >,
+        layout: Layout<1, 1, 1>,
+    }
+
+    #[local]
+    struct Local {
+        matrix: Matrix<DynPin, DynPin, 1, 1>,
+        debouncer: Debouncer<[[bool; 1]; 1]>,
+    }
+
+    #[init]
+    fn init(ctx: init::Context) -> (Shared, Local, init::Monotonics) {
+        info!("Init");
+
+        // clocks
+        let mut resets = ctx.device.RESETS;
+        let mut watchdog = Watchdog::new(ctx.device.WATCHDOG);
+        let clocks = clocks::init_clocks_and_plls(
+            XOSC_CRYSTAL_FREQ,
+            ctx.device.XOSC,
+            ctx.device.CLOCKS,
+            ctx.device.PLL_SYS,
+            ctx.device.PLL_USB,
+            &mut resets,
+            &mut watchdog,
+        )
+        .ok()
+        .unwrap();
+
+        // led
+        let sio = Sio::new(ctx.device.SIO);
+        let pins = rp_pico::Pins::new(
+            ctx.device.IO_BANK0,
+            ctx.device.PADS_BANK0,
+            sio.gpio_bank0,
+            &mut resets,
+        );
+        pins.led.into_push_pull_output().set_high().unwrap();
+
+        let usb_bus = UsbBusAllocator::new(UsbBus::new(
+            ctx.device.USBCTRL_REGS,
+            ctx.device.USBCTRL_DPRAM,
+            clocks.usb_clock,
+            true,
+            &mut resets,
+        ));
+        unsafe {
+            USB_BUS = Some(usb_bus);
+        }
+        let usb_class = keyberon::new_class(unsafe { USB_BUS.as_ref().unwrap() }, ());
+        let usb_dev = keyberon::new_device(unsafe { USB_BUS.as_ref().unwrap() });
+
+        let matrix: Matrix<DynPin, DynPin, 1, 1> = Matrix::new(
+            [pins.gpio13.into_pull_up_input().into()],
+            [pins.gpio14.into_push_pull_output().into()],
+        )
+        .unwrap();
+
+        let layout = Layout::new(&LAYERS);
+        let debouncer = Debouncer::new([[false; 1]; 1], [[false; 1]; 1], 20);
+
+        scan_timer_irq::spawn().unwrap();
+
+        (
+            Shared {
+                usb_dev,
+                usb_class,
+                layout,
+            },
+            Local { matrix, debouncer },
+            init::Monotonics(),
+        )
+    }
+
+    #[task(binds = USBCTRL_IRQ, priority = 3, shared = [usb_dev, usb_class])]
+    fn usb_rx(c: usb_rx::Context) {
+        let mut usb_d = c.shared.usb_dev;
+        let mut usb_c = c.shared.usb_class;
+        usb_d.lock(|d| {
+            usb_c.lock(|c| {
+                if d.poll(&mut [c]) {
+                    c.poll();
+                }
+            })
+        });
+    }
+
+    #[task(priority = 2, shared = [usb_dev, usb_class, layout])]
+    fn handle_event(mut c: handle_event::Context, event: Option<layout::Event>) {
+        match event {
+            None => (),
+            Some(e) => {
+                c.shared.layout.lock(|l| l.event(e));
+                return;
+            }
+        };
+
+        let report: key_code::KbHidReport = c.shared.layout.lock(|l| l.keycodes().collect());
+        if !c
+            .shared
+            .usb_class
+            .lock(|k| k.device_mut().set_keyboard_report(report.clone()))
+        {
+            // info!("not ready");
+            return;
+        }
+        if c.shared.usb_dev.lock(|d| d.state()) != UsbDeviceState::Configured {
+            info!("not configured");
+            return;
+        }
+        while let Ok(0) = c.shared.usb_class.lock(|k| k.write(report.as_bytes())) {}
+    }
+
+    #[task(
+        priority = 1,
+        local = [matrix, debouncer],
+    )]
+    fn scan_timer_irq(c: scan_timer_irq::Context) {
+        let keys_pressed = c.local.matrix.get().unwrap();
+        let deb_events = c.local.debouncer.events(keys_pressed);
+
+        for event in deb_events {
+            info!("event");
+            handle_event::spawn(Some(event)).unwrap();
+        }
+        handle_event::spawn(None).unwrap();
+        scan_timer_irq::spawn().unwrap();
+    }
+}
